1f94817de7a2ccbbf5c4637ac1cdb57a8b0b0602
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-04 17:14:17 -0700

    Update this.players

--- src/game.js
+++ src/game.js
@@ -1,18 +1,41 @@
 class Game {
-  constructor(players, ) {
-    this.players = players;
-    this.gameboard = [
+  constructor() {
+    this.players = {
+      player1: new Player('🌸', 1),
+      player2: new Player('🌼', 2)
+    };
+    this.turn = this.players.player1;
+    this.board = [
       [' ', ' ', ' '],
       [' ', ' ', ' '],
-      [' ', ' ', ' '] ]
+      [' ', ' ', ' ']
+    ]
   }
 
   trackDataForGame() {
+    // for (var i = 0; i < this.board.length; i++) {
+    //   for (var j = 0; j < this.board[i].length; j++) {
+    //     if (this.board[i][j] === ' ') {
+    //       fillInBoard();
+    //     } else {
+    //       return false;
+    //     }
+    //   }
+    // }
+  }
 
+  fillInBoard() {
+    // if(this.turn === this.players.player1) {
+    //
+    // }
   }
 
   whoseTurn() {
-
+    // if (this.turn === this.players.player1) {
+    //   this.turn = this.players.player2;
+    // } else if (this.turn === this.players.player2) {
+    //   this.turn = this.players.player1;
+    // }
   }
 
   checkForWins() {
