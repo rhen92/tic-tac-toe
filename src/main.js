a9adf24458157ece713578802c68c27cb6b26502
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-06 15:15:22 -0700

    Add changeSideBar function

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 var board = document.querySelector('#gameBoard');
 var mainHeading = document.querySelector('#mainHeading');
+var oneWinStatus = document.querySelector('#oneWinStatus');
+var twoWinStatus = document.querySelector('#twoWinStatus');
+
 var ticTacToe = new Game();
 
 board.addEventListener('click', clickHandler);
+window.addEventListener('load', showWins);
 
 function clickHandler() {
   if (event.target.classList.contains('zero')) {
@@ -53,13 +57,28 @@ function displayBoard(event) {
 
 function changeHeading() {
   if (ticTacToe.checkForWins()) {
-    mainHeading.innerText = `${ticTacToe.turn.token} won!`
+    mainHeading.innerText = `${ticTacToe.turn.token} won!`;
+    changeSideBar();
   } else if (!ticTacToe.checkForDraw()) {
-    mainHeading.innerText = 'It\s a draw!'
+    mainHeading.innerText = 'It\s a draw!';
   } else {
     mainHeading.innerText = `It's ${ticTacToe.turn.token}'s turn`;
   }
 }
 
+function changeSideBar() {
+  ticTacToe.saveWinningGame();
+  if(ticTacToe.turn.id === 1) {
+    oneWinStatus.innerText = `${ticTacToe.turn.wins} wins`;
+  } else {
+    twoWinStatus.innerText = `${ticTacToe.turn.wins} wins`;
+  }
+}
 
+// function showWins() {
+//   ticTacToe.players.player1.retrieveWinsFromStorage();
+//   oneWinStatus.innerText = `${ticTacToe.players.player1.wins} wins`;
+//   ticTacToe.players.player2.retrieveWinsFromStorage();
+//   twoWinStatus.innerText = `${ticTacToe.players.player2.wins} wins`;
+// }
 //reset the Game
