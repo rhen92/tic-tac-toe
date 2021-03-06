7f2bfd164bedc97b589fee00c2feb048d49ab26f
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-06 10:59:35 -0700

    Change seeifEmpty function

--- src/main.js
+++ src/main.js
@@ -1,48 +1,57 @@
-//on window load see whose turn it is
-// window.addEventListener('load', function() {
-//
-// })
-
+var board = document.querySelector('#gameBoard');
+var indexZero = document.querySelector('#zero');
+var mainHeading = document.querySelector('#mainHeading');
+var ticTacToe = new Game();
 //player 1 clicks somewhere in the 3X3 grid
-//switch to player 2's turn (whoseTurn method)
+board.addEventListener('click', clickHandler);
+
+function clickHandler() {
+  if (event.target.classList.contains('zero')) {
+    seeIfEmpty(0);
+  }
+}
+
+function seeIfEmpty(value) {
+  ticTacToe.trackDataForGame(value);
+  if (ticTacToe.trackDataForGame(value)) {
+    ticTacToe.fillInBoard(value);
+    displayBoard(event);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// function displayBoard(event) {
+//   event.target.innerText = `${ticTacToe.turn.token}`;
+//   ticTacToe.whoseTurn();
+//   changeHeading();
+// }
+
 //rewrite heading of whose turn it is at top of page
+// function changeHeading() {
+//   mainHeading.innerText = `It's ${ticTacToe.turn.token}'s turn`;
+// }
+
 //player 2 clicks somewhere in the 3X3 grid (trackDataForGame method?)
 //check to make sure its a valid click and not overwrite player 1's clicks (trackDataForGame method?)
 //check for possible wins (checkForWins method)
 //check for possible draws (checkForDraw method)
 //keep going with the back and forths until either check for wins comes back true or check for draws comes back true
 //reset the Game
-var ticTacToe = new Game();
-
-ticTacToe.fillInBoard(0);
-ticTacToe.whoseTurn();
-ticTacToe.fillInBoard(2);
-ticTacToe.whoseTurn();
-ticTacToe.fillInBoard(1);
-ticTacToe.whoseTurn();
-ticTacToe.fillInBoard(3);
-ticTacToe.whoseTurn();
-ticTacToe.fillInBoard(4);
-ticTacToe.whoseTurn();
-ticTacToe.fillInBoard(6);
-ticTacToe.whoseTurn();
-ticTacToe.fillInBoard(5);
-ticTacToe.whoseTurn();
-ticTacToe.fillInBoard(8);
-ticTacToe.whoseTurn();
-ticTacToe.fillInBoard(7)
 
 
 
-ticTacToe.checkForWins();
-if(!ticTacToe.checkForWins()) {
-  ticTacToe.checkForDraw();
-}
+
+// ticTacToe.checkForWins();
+// if(!ticTacToe.checkForWins()) {
+//   ticTacToe.checkForDraw();
+// }
 
 // ticTacToe.trackDataForGame(0);
-// if(ticTacToe.trackDataForGame(0)) {
+// if (ticTacToe.trackDataForGame(0)) {
 //   ticTacToe.fillInBoard(0);
-//   ticTacToe.whoseTurn();
 // } else {
-//   console.log('Choose another spot');
+//   return;
 // }
+// ticTacToe.whoseTurn();
